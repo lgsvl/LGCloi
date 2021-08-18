e9521a7694697764f39aefb14f17d02d7c7d34b1
Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
2021-08-17 18:03:11 -0700

    Add SVLSimulator license to cs files

--- RobotController.cs
+++ RobotController.cs
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2021 LG Electronics, Inc.
  *
- * This software contains code licensed as described in LICENSE.
+ * This software contains code licensed as described in LICENSE-SVLSimulator.
  *
  */
 
